574bfa0558dd19d1aad203e9956a0b7cdc2432f4
Jared Harbison <jared.harbison@gmail.com>
2019-02-26 16:53:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,12 +8,12 @@ class App < Sinatra::Base
   get "/square/:number" do
     @number = params[:number]
     @result = @number.to_i * @number.to_i
-    @result.to_s 
+    @result.to_s
   end
   get "/say/:number/:phrase"
   @number = params[:number]
   @phrase = params[:phrase]
   @return_string = ""
-  @number.to_i.times do 
+  @number.to_i.times do
     @return_string += "#{@phrase}"
 end
