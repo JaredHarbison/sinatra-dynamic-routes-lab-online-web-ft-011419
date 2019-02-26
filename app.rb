f581f383e891f22fecfe7314ab76428ede478c86
Jared Harbison <jared.harbison@gmail.com>
2019-02-26 17:03:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,10 +34,10 @@ class App < Sinatra::Base
   get "/:operation/:number1/:number2" do
     @number1 = params[:number1]
     @number2 = params[:number2]
-    if 
-    elsif 
-    else 
-    else 
+    if
+    elsif
+    else
+    else
     end 
   end
 
